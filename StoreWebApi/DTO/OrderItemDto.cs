51c898b0399aa5e710d8397ed07743da7022f49f
amaralanany12345 <Alanany09@gmail.com>
2026-05-15 14:31:32 +0300

    update the order contoller

---
+++ StoreWebApi/DTO/OrderItemDto.cs
@@ -0,0 +1,9 @@
+﻿namespace StoreWebApi.DTO
+{
+    public class OrderItemDto
+    {
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+        public string ItemName { get; set; }
+    }
+}
